3b26bc0fc276e16bffc1e2d7eddf513454ce3a00
Reuben deVries <reubendevries@gmail.com>
2024-06-04 19:49:31 -0700

    finished project 01 - plant a tree.

---
+++ java/project-01/planting-a-tree/Tree.java
@@ -0,0 +1,27 @@
+public class Tree {
+    public static void main(String[] args) {
+        // We are going to introduce ourseleves and print a tree to the screen.
+
+        System.out.println("Hey there, I\’m Reuben and I\’m learning to code in Java!");
+        System.out.println("I\'m going to plant a tree today!");
+        System.out.println("I\'m ready to get my hands dirty!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  "); 
+        System.out.println("  *  ");
+
+        System.out.println("* ** *");
+        System.out.println("******");
+        System.out.println(" **** ");
+        System.out.println("  ** ");
+        System.out.println("  ** ");
+
+        System.out.println("*****"); 
+        System.out.println("*****");
+        System.out.println("*****"); 
+        System.out.println("  *  ");   
+        System.out.println("  *  ");   
+        System.out.println("#######");
+    }
+}
